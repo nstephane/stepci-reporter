5f8a575eaf9e1da83661202f631a4b472c3e1e18
Stephane Martin <nstephanemartin@gmail.com>
2024-11-13 05:16:33 +0200

    clean up repos

--- dist/api/server.d.ts
+++
@@ -1,9 +0,0 @@
-import { StorageProvider } from '../storage/base';
-export declare class ReporterAPI {
-    private storage;
-    private app;
-    constructor(storage: StorageProvider);
-    private setupMiddleware;
-    private setupRoutes;
-    start(port: number): Promise<unknown>;
-}
